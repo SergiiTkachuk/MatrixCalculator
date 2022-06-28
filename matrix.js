defcaee788dee805b01c796c5f3152f57c917afc
SergiiTkachuk <sergiytkachuk29@gmail.com>
2022-06-28 15:49:36 +0300

    add test matrixes

--- matrix.js
+++ matrix.js
@@ -1,10 +1,39 @@
-"use strict";
+'use strict';
 
-const readline = require("readline");
+const readline = require('readline');
 
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
-  prompt: "> ",
+  prompt: '> ',
+
 });
+
+const matrixA = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+];
+const matrixB = [
+  [2, 3, 4],
+  [5, 6, 7],
+  [1, 8, 9],
+];
+const a = 2;
+
+const matrixC = [
+  [1, 2, 3, 7],
+  [4, 5, 6, 8],
+];
+const matrixD = [
+  [2, 3],
+  [5, 6],
+  [1, 8],
+  [4, 9],
+];
+const matrixDet = [
+  [1, 3, 7],
+  [2, -1, 4],
+  [5, 0, 2],
+];
 
